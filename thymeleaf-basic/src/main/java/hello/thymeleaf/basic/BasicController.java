faefbb8d2906b5a6732c9bf5faabe700fb24fa10
Kooky <kooky1001@gmail.com>
2023-04-16 12:09:17 +0900

    주석/블록

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -130,4 +130,16 @@ public class BasicController {
         addUsers(model);
         return "basic/condition";
     }
+
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Thymeleaf!");
+        return "basic/comments";
+    }
+
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
 }
