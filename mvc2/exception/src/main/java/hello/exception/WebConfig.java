89d177ca87d992b67c3b394c6aa5976481c87b40
Kooky <kooky1001@gmail.com>
2024-05-27 17:19:36 +0900

    HandlerExceptionResolver 적용

--- mvc2/exception/src/main/java/hello/exception/WebConfig.java
+++ mvc2/exception/src/main/java/hello/exception/WebConfig.java
@@ -1,13 +1,17 @@
 package hello.exception;
 
+import java.util.List;
+
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import hello.exception.filter.LogFilter;
 import hello.exception.interceptor.LogInterceptor;
+import hello.exception.resolver.MyHandlerExceptionResolver;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 
@@ -32,4 +36,8 @@ public class WebConfig implements WebMvcConfigurer {
 		return filterRegistrationBean;
 	}
 
+	@Override
+	public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+		resolvers.add(new MyHandlerExceptionResolver());
+	}
 }
