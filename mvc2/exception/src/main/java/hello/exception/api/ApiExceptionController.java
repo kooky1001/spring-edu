3377eaee1313393bc09f1064efae4cc031d8a004
Kooky <kooky1001@gmail.com>
2024-05-29 10:31:14 +0900

    DefaultHandlerExceptionResolver 확인

--- mvc2/exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ mvc2/exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -3,6 +3,7 @@ package hello.exception.api;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -15,6 +16,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 public class ApiExceptionController {
+
+	@GetMapping("/api/default-handler-ex")
+	public String defaultException(@RequestParam Integer data) {
+		return "ok";
+	}
 
 	@GetMapping("/api/response-status-ex1")
 	public String responseStatusEx1() {
