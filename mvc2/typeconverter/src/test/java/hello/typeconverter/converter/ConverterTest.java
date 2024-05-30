2e8164574f9fad7229b53256508ab7c9c50d17d4
Kooky <kooky1001@gmail.com>
2024-05-30 15:07:18 +0900

    Converter로 IpPort <-> String 테스트

--- mvc2/typeconverter/src/test/java/hello/typeconverter/converter/ConverterTest.java
+++ mvc2/typeconverter/src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.*;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import hello.typeconverter.type.IpPort;
+
 class ConverterTest {
 
 	@Test
@@ -19,5 +21,21 @@ class ConverterTest {
 		IntegerToStringConverter converter = new IntegerToStringConverter();
 		String result = converter.convert(10);
 		assertThat(result).isEqualTo("10");
+	}
+
+	@Test
+	void stringToIpPort() {
+		StringToIpPortConverter converter = new StringToIpPortConverter();
+		String source = "127.0.0.1:8080";
+		IpPort result = converter.convert(source);
+		assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
+	}
+
+	@Test
+	void ipPortToString() {
+		IpPortToStringConverter converter = new IpPortToStringConverter();
+		IpPort source = new IpPort("127.0.0.1", 8080);
+		String result = converter.convert(source);
+		assertThat(result).isEqualTo("127.0.0.1:8080");
 	}
 }
