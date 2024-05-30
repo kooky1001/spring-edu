ae06253a95b714ea1e1bc650e37d894e16a7179c
Kooky <kooky1001@gmail.com>
2024-05-30 16:17:19 +0900

    ConversionService 적용 테스트

--- mvc2/typeconverter/src/test/java/hello/typeconverter/converter/ConverterTest.java
+++ mvc2/typeconverter/src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -2,7 +2,6 @@ package hello.typeconverter.converter;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import hello.typeconverter.type.IpPort;
