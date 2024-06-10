8e27a5441b55cce7cead9ff752e4e3d16cff7f57
Kooky <kooky1001@gmail.com>
2024-06-10 14:52:05 +0900

    HikariDataSource 사용 커넥션풀에서 커넥션 획득

--- db1/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ db1/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -11,6 +11,8 @@ import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import com.zaxxer.hikari.HikariDataSource;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -29,6 +31,20 @@ class ConnectionTest {
 		//DriverManagerDataSource - 항상 새로운 커넥션 획득
 		DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
 		useDataSource(dataSource);
+	}
+
+	@Test
+	void dataSourceConnectionPool() throws SQLException, InterruptedException {
+		//커넥션 풀링: HikariProxyConnection(Proxy -> JdbcConnection(Target)
+		HikariDataSource dataSource = new HikariDataSource();
+		dataSource.setJdbcUrl(URL);
+		dataSource.setUsername(USERNAME);
+		dataSource.setPassword(PASSWORD);
+		dataSource.setMaximumPoolSize(10);
+		dataSource.setPoolName("myPool");
+
+		useDataSource(dataSource);
+		Thread.sleep(1000);
 	}
 
 	private void useDataSource(DataSource dataSource) throws SQLException {
