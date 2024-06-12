5940bf84004ce0a0356b1223ccbe8228395f2933
Kooky <kooky1001@gmail.com>
2024-06-12 17:44:51 +0900

    트랜잭션 적용하여 계좌이체 예외 테스트

--- db1/jdbc/src/test/java/hello/jdbc/service/MemberServiceV2Test.java
+++ db1/jdbc/src/test/java/hello/jdbc/service/MemberServiceV2Test.java
@@ -13,24 +13,25 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV1;
+import hello.jdbc.repository.MemberRepositoryV2;
 
 /**
  * 기본 동작, 트랜잭션이 없어서 문제 발생
  */
-class MemberServiceV1Test {
+class MemberServiceV2Test {
 
 	public static final String MEMBER_A = "memberA";
 	public static final String MEMBER_B = "memberB";
 	public static final String MEMBER_EX = "ex";
 
-	private MemberRepositoryV1 memberRepository;
-	private MemberServiceV1 memberService;
+	private MemberRepositoryV2 memberRepository;
+	private MemberServiceV2 memberService;
 
 	@BeforeEach
 	void setUp() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
-		memberRepository = new MemberRepositoryV1(dataSource);
-		memberService = new MemberServiceV1(memberRepository);
+		memberRepository = new MemberRepositoryV2(dataSource);
+		memberService = new MemberServiceV2(dataSource, memberRepository);
 	}
 
 	@AfterEach
@@ -77,8 +78,8 @@ class MemberServiceV1Test {
 		Member findMemberA = memberRepository.findById(memberA.getMemberId());
 		Member findMemberEx = memberRepository.findById(memberEx.getMemberId());
 
-		//memberA의 돈만 2000원 줄었고, ex의 돈은 10000원 그대로이다.
-		assertThat(findMemberA.getMoney()).isEqualTo(8000);
+		//memberA의 돈이 롤백 되어야함
+		assertThat(findMemberA.getMoney()).isEqualTo(10000);
 		assertThat(findMemberEx.getMoney()).isEqualTo(10000);
 	}
 }
